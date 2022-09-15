d78a28569d1a15b2f6a09206fdd782838b861896
Pavel <vasilyev.x@mail.com>
2022-09-15 14:03:02 +0300

    Homework

--- src/ServiceStation.java
+++ src/ServiceStation.java
@@ -1,1 +1,29 @@
-public class ServiceStation {}
+public class ServiceStation {
+    public void check(Car car) {
+        printServiceMsg(car);
+        for (int i = 0; i < car.getWheelsCount(); i++) {
+            car.updateTyre();
+        }
+        car.checkEngine();
+    }
+
+    public void check(Truck truck) {
+        printServiceMsg(truck);
+        for (int i = 0; i < truck.getWheelsCount(); i++) {
+            truck.updateTyre();
+        }
+        truck.checkEngine();
+        truck.checkTrailer();
+    }
+
+    public void check(Bicycle bicycle) {
+        printServiceMsg(bicycle);
+        for (int i = 0; i < bicycle.getWheelsCount(); i++) {
+            bicycle.updateTyre();
+        }
+    }
+
+    private void printServiceMsg(Transport transport) {
+        System.out.println("Обслуживаем " + transport.getModelName());
+    }
+}
